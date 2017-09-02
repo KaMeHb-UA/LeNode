5ada4d3fede79c2b3e87206882b939957894269c
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-09-02 11:06:19 +0300

    Console pretty print cross platform update

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-var spawn = require('child_process').spawn;
-    
-if (/^win/.test(process.platform)) spawn('cmd', ['/s', '/c', 'node nodemon | cmdcolor'],{stdio:'inherit'}); else spawn('node', ['nodemon'],{stdio:'inherit'});
+var spawn = require('child_process').spawn,
+    colors = require('colors'),
+    nodemon = require('nodemon'),
+    dateTime = require('node-datetime'),
+    projectName = (function(a){a=a.split('/');a=a[a.length-1];a=a.split('\\');return a[a.length-1];})(process.cwd());
+nodemon({ script: 'server-main.js' }).on('restart', function(fn){
+    console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.yellow + ' server restarted due to: '.yellow + fn);
+});
