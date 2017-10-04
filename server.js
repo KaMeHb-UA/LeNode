9472aa35c6a5998bb6e065d294ac17efa848ec01
Unknown <blade.whiteblack@gmail.com>
2017-10-04 20:04:50 +0300

    Release commit 2.0.0-a
    Will pass several tests

--- server.js
+++ server.js
@@ -3,11 +3,44 @@ var fs = require('fs'),
     dateTime = require('node-datetime'),
     nodemon = require('nodemon'),
     projectFolder = (a=>{a.pop();return a.join('/')})(process.mainModule.filename.split('/')),
-    projectName = (a=>{return a[a.length-1]})(projectFolder.split('/'));
-(function start(){
+    projectName = (a=>{return a[a.length-1]})(projectFolder.split('/')),
+    rimraf = require('rimraf'),
+    mv = require('mv'),
+    unzip = require('unzip'),
+    mkdirp = require('mkdirp'),
+    request = require('request');
+(()=>{
+    var app = {
+        log : (heading, text)=>{
+            console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.green + ' ' + heading.green + ': '.green + text);
+        },
+        warn : (heading, text)=>{
+            console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.yellow + ' ' + heading.yellow + ': '.yellow + text);
+        },
+        err : (heading, text)=>{
+            console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.red + ' ' + heading.red + ': '.red + text);
+        }
+    }
+    function searchForUpdate(currentVersion, callback){
+        request({
+            url: 'https://api.github.com/repos/KaMeHb-UA/LeNode/releases/latest',
+            headers: {
+              'User-Agent': 'LeNode ' + currentVersion
+            },
+            json: true
+          }, (err, res, body) => {
+            if (err) callback(err, null);
+            else callback(null, 
+            {
+                latest: body.tag_name,
+                url: body.assets[0].browser_download_url
+            });
+          });
+    }
     fs.readdir(projectFolder + '/app', (e, files)=>{
         if(e){
-            console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.red + ' server: cannon get current app. Check your ./app folder and try again or just reinstall the app. Full stack is there:\n'.red);
+            app.err('server', 'cannon get current app. Check your ./app folder and try again or just reinstall the app. Full stack is there:\n');
+            //console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.red + ' server: cannon get current app. Check your ./app folder and try again or just reinstall the app. Full stack is there:\n'.red);
             throw e;
         } else {
             let maxV = '0.0.0';
@@ -31,11 +64,53 @@ var fs = require('fs'),
                     (curVersion.version == maxVersion.version && curVersion.major == maxVersion.major && curVersion.minor == maxVersion.minor && curVersion.revision > maxVersion.revision)
                 ) maxV = version;
             });
-            console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.green + ' server:'.green + ' loading LeNode v.' + maxV);
-            nodemon({
-                script: projectFolder + '/app/' + maxV + '/server-main.js'
-            }).on('restart', function(fn){
-                console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.yellow + ' server restarted due to: '.yellow + fn);
+            //console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.green + ' server:'.green + ' loading LeNode v.' + maxV);
+            function startServer(){
+                app.log('server', 'loading LeNode v.' + maxV);
+                return nodemon({
+                    script: projectFolder + '/app/' + maxV + '/server.js'
+                }).on('restart', function(fn){
+                    app.warn('server restarted due to', fn);
+                    //console.log(dateTime.create().format('[d-m-y H:M:S]').cyan + ' ' + projectName.yellow + ' server restarted due to: '.yellow + fn);
+                });
+            }
+            var startedScript = startServer();
+            searchForUpdate(maxV, (err,data)=>{
+                app.log('server', 'searching updates');
+                if(!err && data.latest != maxV){
+                    rimraf(projectFolder + '/.tmp/', (e)=>{
+                        if (!e){
+                            let versionFolder = projectFolder + '/.tmp/' + data.latest;
+                            mkdirp(versionFolder, (r)=>{
+                                if (!r){
+                                    let file = fs.createWriteStream(versionFolder + '.zip');
+                                    app.log('server', 'downloading update... This may take awhile');
+                                    request(data.url).pipe(file).on('finish', ()=>{
+                                        file.close();
+                                        fs.createReadStream(versionFolder + '.zip').pipe(unzip.Extract({
+                                            path: versionFolder
+                                        })).on('close', ()=>{
+                                            mv(versionFolder + '/app', projectFolder + '/app', ()=>{
+                                                rimraf(projectFolder + '/.tmp/', ()=>{
+                                                    var timeout = 1000;
+                                                    app.warn('server', 'restarting due to update...');
+                                                    startedScript.emit('quit');
+                                                    app.log('server', 'will be started after ' + timeout + 'ms...');
+                                                    setTimeout(()=>{
+                                                        startServer().once('exit', ()=>{process.exit()})
+                                                    }, timeout);
+                                                });
+                                            });
+                                        });
+                                    });
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    app.log('server info', 'your server has an actual LeNode version');
+                    startedScript.once('exit', ()=>{process.exit()})
+                }
             });
         }
     })
